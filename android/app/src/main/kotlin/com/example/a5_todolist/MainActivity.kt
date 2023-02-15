8f2d4bfb8b71385912e5a754db1344b4938ae3e0
Cyril-lamirand <contact.cyril1@gmail.com>
2023-02-15 22:19:35 +0100

    [Initialization] Files & Structure

---
+++ android/app/src/main/kotlin/com/example/a5_todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a5_todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+// Dart default
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+// Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+// Screens
+import 'views/welcome.dart';
+import 'views/register.dart';
+import 'views/login.dart';
+import 'views/home.dart';
+import 'views/profile.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'A5 Flutter app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder> {
+        'welcome_screen': (context) => Welcome(),
+        'registration_screen': (context) => Register(),
+        'login_screen': (context) => Login(),
+        'profile_screen': (context) => Profile(),
+        'home_screen': (context) => Home()
+      },
+      initialRoute: 'welcome_screen',
+    );
+  }
+}
